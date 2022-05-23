6e53455f861c3a79309ffc5acf3dde784d57c97e
keepclm <104723698+keepclm@users.noreply.github.com>
2022-05-23 18:17:33 -0400

    Add files via upload

---
+++ mail/contact_me.php
@@ -0,0 +1,24 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+	
+// Create the email and send the message
+$to = 's.fpe0110@gmai.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Contacto atravez de tu pagina:  $name";
+$email_body = "haz recibido un correo de contacto.\n\n"."Aquí los detalles:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
